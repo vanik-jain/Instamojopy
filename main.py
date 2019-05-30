dc3e491f06a890a3cae196266dee89a5fcac9185
vanik-jain <31779923+vanik-jain@users.noreply.github.com>
2019-05-31 00:07:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,28 +10,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    client_id = "CBzDgynYEWxICcKYHxWSQfUvoY5inUnQqqeFcICe"
+    client_secret = "TykXXhbl06Yp0ifGrsdWGabKeeBU5nhEC29ivceMhQpz78BM5AJ4l1G5ShfT3rYAcnndBfFvkOtCNjfsYUgUadoNW3FEYP1LVMIxfdmhdAZ9ZhmsBoSQGPeH1Q1ZGhA9"
+    env = "production"
 
-
-
-client_id = "CBzDgynYEWxICcKYHxWSQfUvoY5inUnQqqeFcICe"
-client_secret = "TykXXhbl06Yp0ifGrsdWGabKeeBU5nhEC29ivceMhQpz78BM5AJ4l1G5ShfT3rYAcnndBfFvkOtCNjfsYUgUadoNW3FEYP1LVMIxfdmhdAZ9ZhmsBoSQGPeH1Q1ZGhA9"
-env = "production"
-
-if (client_id.startswith("test")):
+    if (client_id.startswith("test")):
     url = "https://test.instamojo.com/oauth2/token/"
     env = "test"
 
-payload = "grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret
-headers = {
+    payload = "grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret
+    headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
     }
 
-response = requests.request("POST",  "https://test.instamojo.com/oauth2/token/", data=payload, headers=headers)
-token = env + json.loads(response.text)["access_token"]
-print(token)
+    response = requests.request("POST",  "https://test.instamojo.com/oauth2/token/", data=payload, headers=headers)
+    token = env + json.loads(response.text)["access_token"]
+
+    return token
+
 
 
 
