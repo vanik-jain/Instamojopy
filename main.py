3f30f7ed8606580120d9821b0ab8d42e1efb4e5e
vanik-jain <31779923+vanik-jain@users.noreply.github.com>
2019-05-31 01:10:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def hello():
     }
 
     response = requests.request("POST", "https://api.instamojo.com/oauth2/token/", data=payload, headers=headers)
-    token = env + str(json.loads(response.text))
+    token = env + json.loads(response.text)["access_token"]
 
     return token
 
